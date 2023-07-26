cf7d9e039a4408e41f32f901bb7471dba25946d8
Gavin Peng <gpeng@oicr.on.ca>
2023-07-25 20:23:36 -0400

    change calculate.sh in case optional outputs do not exist

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -7,4 +7,4 @@ cd $1
 
 ls | sed 's/.*\.//' | sort | uniq -c
 find . -name '*.tsv'  | xargs md5sum | sort
-find . -name '*.vcf.gz' | xargs zcat | grep -v ^# | md5sum | sort
+for i in $(find . -name '*.vcf.gz'); do if [ -f $i ]; then zcat $i | grep -v ^# | md5sum; fi; done
