06d7af3343d53f653ac3c5210022c4f050c69beb
pruzanov <pruzanov@oicr.on.ca>
2026-01-09 15:43:54 -0500

    Disabled md5sum for vcf files as the RT currently generates empty (header-only) vcfs

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -7,4 +7,4 @@ cd $1
 
 ls | sed 's/.*\.//' | sort | uniq -c
 find . -name '*.tsv'  | xargs md5sum | sort
-for i in $(find . -name '*.vcf.gz'); do if [ -f $i ]; then zcat $i | grep -v ^# | md5sum; fi; done
+#for i in $(find . -name '*.vcf.gz'); do if [ -f $i ]; then zcat $i | grep -v ^# | md5sum; fi; done
